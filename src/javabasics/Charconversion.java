43caf2c29ea506edb720ba71ad24385d5b708f5f
Balkrushna <Balkrushna>
2022-12-09 12:31:39 +0530

    s

--- src/javabasics/Charconversion.java
+++ src/javabasics/Charconversion.java
@@ -3,6 +3,7 @@ package javabasics;
 public class Charconversion {
 
 	public static void main(String[] args) {
+		System.out.println("main method");
 		System.out.println("after clone the changes are done");
 		System.out.println('a'+3);
 	   System.out.println(3+'a');
