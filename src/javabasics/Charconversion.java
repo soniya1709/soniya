f78c043b216e2b9e13e90d112fbfa769c7f63d32
soniya1709 <88476209+soniya1709@users.noreply.github.com>
2022-12-11 21:52:08 +0530

    Update Charconversion.java

--- src/javabasics/Charconversion.java
+++ src/javabasics/Charconversion.java
@@ -5,10 +5,7 @@ public class Charconversion {
 	public static void main(String[] args) {
 		System.out.println("after clone the changes are done");
 		System.out.println('a'+3);
-	   System.out.println(3+'a');
-	   
-	   System.out.println("soniya creation");
-	   
+	   System.out.println(3+'a');	   
 	}
 	
 }
